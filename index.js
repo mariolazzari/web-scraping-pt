06546f62effd4b82b7c06b783bca32df78bbbdfc
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-11 20:21:11 +0200

    Added getTechs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   // browser init
   const browser = await puppeteer.launch({
-    headless: false,
+    // headless: false,
   });
   // open new tab
   const page = await browser.newPage();
@@ -11,6 +11,7 @@ const puppeteer = require("puppeteer");
   await page.goto("https://machadopedro.com/");
   // take screenshot
   await page.screenshot({ path: "myWwbsite.png" });
+
   // get page paragraph
   const getParagraph = await page.evaluate(() => {
     const pgTag = document.querySelector(".col-lg-6.intro-content p");
@@ -18,8 +19,19 @@ const puppeteer = require("puppeteer");
     //return pgTag.innerHTML;
     return pgTag.innerText;
   });
-
   console.log(getParagraph);
+
+  // get technologies
+  const getTechnologies = await page.evaluate(() => {
+    let techs = [];
+
+    const tags = document.querySelectorAll(".row.back-end ul li");
+    tags.forEach(tag => techs.push(tag.innerText));
+
+    return techs;
+  });
+
+  console.log(getTechnologies);
 
   // close browser
   await browser.close();
